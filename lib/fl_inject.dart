4189ca34c59f468acbee0fc678344adad0eedcdf
binhnv8 <binhnv8>
2026-04-22 13:47:55 +0700

    Update export file

--- lib/fl_inject.dart
+++ lib/fl_inject.dart
@@ -1,3 +1,8 @@
 library;
 
 export 'src/mini_app_code_generator.dart';
+export 'src/annotations/mini_app_annotations.dart';
+export 'src/base/base_generate.dart';
+export 'src/base/base_generate_model.dart';
+export 'src/generators/mini_app_merged_di_gen.dart';
+export 'src/generators/mini_app_screen_gen.dart';
