bfec4e183bed36295fbbe025335923475f9f005c
binhnv8 <binhnv8>
2026-04-22 16:13:10 +0700

    Update example

--- example/flinject_example.dart
+++ example/flinject_example.dart
@@ -1,6 +1,4 @@
 import 'package:fl_inject/fl_inject.dart';
-import 'package:fl_inject/src/generators/mini_app_merged_di_gen.dart';
-import 'package:fl_inject/src/generators/mini_app_screen_gen.dart';
 
 void main() {
   generateInjectAnnotation(
